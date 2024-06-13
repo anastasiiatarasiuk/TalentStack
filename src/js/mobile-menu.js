8b3a8d69839885a3fd65579750fe1300be94b19e
NataliaShtefan <153209330+NataliaShtefan@users.noreply.github.com>
2024-06-13 15:01:38 +0200

    header
    js mob
    js back
    class

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,13 +2,13 @@ const btnMenu = document.querySelector(".menu-btn");
 const btnClose = document.querySelector(".btn-close");
 const menu = document.querySelector(".mobile-container");
 const menuLinks = document.querySelectorAll(".menu-item-nav");
-
+const body = document.querySelector("body")
 
 const toggleMenu = () => {
     menu.classList.toggle("show");
-btnMenu.classList.toggle("disables");
+btnMenu.classList.toggle("disabled");
 btnClose.classList.toggle("show");
-
+body.classList.toggle("no-skroll")
 };
 
 btnMenu.addEventListener("click", toggleMenu);
@@ -18,7 +18,25 @@ menuLinks.forEach(link => {
     link.addEventListener("click", () => {
         menu.classList.remove("show");
         btnClose.classList.remove("show");
-        btnMenu.classList.remove("disables");
-    
+        btnMenu.classList.remove("disabled");
+    body.classList.remove("no-skroll");
     });
 });
+
+// switch
+
+const checkbox = document.querySelector(".switch-checkbox");
+const checkboxMob = document.querySelector(".switch-checkbox-mob");
+
+const changeBackGround = () => {
+    if (checkbox.checked || checkboxMob.checked) {
+        document.body.style.backgroundColor = "#292929";
+        document.body.style.color = "#f0f0f0";
+    } else {
+        document.body.style.backgroundColor = "#f0f0f0";
+        document.body.style.color = "#292929";
+    }
+};
+
+checkbox.addEventListener("change", changeBackGround);
+checkboxMob.addEventListener("change", changeBackGround);
