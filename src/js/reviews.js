081bf430d6516a4f41a13d872ca35e700746a91a
ann-shevchenko <154280836+ann-shevchenko@users.noreply.github.com>
2024-06-14 23:10:30 +0200

    code fix and optimization

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import { Keyboard, Mousewheel, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import axios from 'axios';
 import iziToast from 'izitoast';
@@ -12,7 +12,7 @@ function showErrorToast(message) {
     image: '../img/bi_x-octagon.svg',
     messageColor: '#FFF',
     position: 'topRight',
-    backgroundColor: '#E74A3B',
+    backgroundColor: '#d66c66',
     maxWidth: '472px',
     imageWidth: 24,
   });
@@ -28,105 +28,100 @@ function renderReviewItem(item) {
       </li>`;
 }
 
-const reviewList = document.querySelector('.reviews-list');
+const prevButton = document.querySelector('.swiper-button-prev');
+const nextButton = document.querySelector('.swiper-button-next');
 
-function renderReviewList(items) {
-  if (items && items.length) {
-    return items.map(renderReviewItem).join('');
+function createReviewList(items) {
+  const reviewList = document.querySelector('.reviews-list');
+  const notFound = document.querySelector('.reviews-not-found');
+
+  items = items || [];
+  if (items.length) {
+    reviewList.innerHTML = items.map(renderReviewItem).join('');
+  } else {
+    prevButton.disabled = true;
+    nextButton.disabled = true;
+    reviewList.classList.add('hidden');
+    notFound.classList.remove('hidden');
   }
-  reviewList.style.justifyContent = 'center';
-  return  '<div class="reviews-not-found"><p>Not found</p></div>';
 }
 
+function initSlider() {
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Keyboard, Mousewheel],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoHeight: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 32,
+      },
+      1280: {
+        slidesPerView: 2,
+        spaceBetween: 32,
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    mousewheel: {
+      invert: false,
+      forceToAxis: true,
+    },
+  });
+
+  const slides = document.querySelectorAll('.swiper-slide');
+  slides.forEach((slide, index) => {
+    slide.addEventListener('focus', () => {
+      swiper.slideTo(index);
+    });
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Tab') {
+      const activeSlide = document.activeElement;
+      if (activeSlide && activeSlide.classList.contains('swiper-slide')) {
+        if (event.shiftKey) {
+          swiper.slidePrev();
+        } else {
+          swiper.slideNext();
+        }
+        event.preventDefault();
+        const newActiveSlide = document.querySelector('.swiper-slide-active');
+        if (newActiveSlide) {
+          newActiveSlide.focus();
+        }
+      }
+    }
+  });
+
+
+  swiper.on('slideChange', () => {
+    prevButton.disabled = swiper.isBeginning;
+    nextButton.disabled = swiper.isEnd;
+  });
+}
 
 
 axios.get('https://portfolio-js.b.goit.study/api/reviews')
   .then(response => {
     if (response.status === 200) {
-      reviewList.innerHTML = renderReviewList(response.data);
-
-      const swiper = new Swiper('.swiper', {
-        modules: [Navigation, Keyboard, Mousewheel],
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        autoHeight: true,
-        breakpoints: {
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 32,
-          },
-          1280: {
-            slidesPerView: 2,
-            spaceBetween: 32,
-          },
-        },
-        keyboard: {
-          enabled: true,
-          onlyInViewport: true,
-        },
-        mousewheel: {
-          invert: false,
-          forceToAxis: true,
-        },
-      });
-
-      const slides = document.querySelectorAll('.swiper-slide');
-      slides.forEach((slide, index) => {
-        slide.addEventListener('focus', () => {
-          swiper.slideTo(index);
-        });
-      });
-
-      document.addEventListener('keydown', function(event) {
-        if (event.key === 'Tab') {
-          const activeSlide = document.activeElement;
-          if (activeSlide && activeSlide.classList.contains('swiper-slide')) {
-            if (event.shiftKey) {
-              swiper.slidePrev();
-            } else {
-              swiper.slideNext();
-            }
-            event.preventDefault();
-            const newActiveSlide = document.querySelector('.swiper-slide-active');
-            if (newActiveSlide) {
-              newActiveSlide.focus();
-            }
-          }
-        }
-      });
-
-      swiper.on('slideChange', function () {
-        updateNavigationButtons();
-      });
-
-      function updateNavigationButtons() {
-        const prevButton = document.querySelector('.swiper-button-prev');
-        const nextButton = document.querySelector('.swiper-button-next');
-
-        if (swiper.isBeginning) {
-          prevButton.disabled = true;
-        } else {
-          prevButton.disabled = false;
-        }
-
-        if (swiper.isEnd) {
-          nextButton.disabled = true;
-        } else {
-          nextButton.disabled = false;
-        }
-      }
+      createReviewList(response.data);
+      initSlider();
 
     } else {
       showErrorToast('Sorry, something went wrong. Try one more time.');
-      reviewList.innerHTML = '<p class="reviews-not-found">Not found</p>';
+      createReviewList([]);
     }
   })
   .catch(() => {
-      showErrorToast('Sorry, something went wrong. Try one more time.');
-      reviewList.style.justifyContent = 'center';
-      reviewList.innerHTML = '<p class="reviews-not-found">Not found</p>';
+    showErrorToast('Sorry, something went wrong. Try one more time.');
+    createReviewList([]);
 
   });
 
