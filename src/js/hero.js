0a6392c05673b4a7342e974ac84f17ec8bd63457
RedisculuS <Altunina311@gmail.com>
2024-06-15 21:38:30 +0300

    1st commit in new "Hero" branch
    fixing bugs

---
+++ src/js/hero.js
@@ -0,0 +1,5 @@
+import accountImgFirst from '../img/hero/account-img1x.png';
+import accountImgSecond from '../img/hero/account-img2x.png';
+
+document.querySelector('.accountImg').src = accountImgFirst;
+document.querySelector('.accountImg').srcset = accountImgSecond;
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,56 +1,54 @@
-const btnMenu = document.querySelector(".menu-btn");
-const btnClose = document.querySelector(".btn-close");
-const backdrob = document.querySelector(".mobile-container");
-const menuLinks = document.querySelectorAll(".menu-item-nav");
-const body = document.querySelector("body")
+const btnMenu = document.querySelector('.menu-btn');
+const btnClose = document.querySelector('.btn-close');
+const backdrob = document.querySelector('.mobile-container');
+const menuLinks = document.querySelectorAll('.menu-item-nav');
+const body = document.querySelector('body');
+const socialBoxHero = document.querySelector('.social-list-box');
+const greenBoxHero = document.querySelector('.green-box');
 
 const toggleMenu = () => {
-  backdrob.classList.toggle("show");
-btnMenu.classList.toggle("disabled");
-btnClose.classList.toggle("show");
-body.classList.toggle("no-scroll")
+  backdrob.classList.toggle('show');
+  btnMenu.classList.toggle('disabled');
+  btnClose.classList.toggle('show');
+  body.classList.toggle('no-scroll');
 };
 
-btnMenu.addEventListener("click", toggleMenu);
-btnClose.addEventListener("click", toggleMenu);
-
+btnMenu.addEventListener('click', toggleMenu);
+btnClose.addEventListener('click', toggleMenu);
 
 menuLinks.forEach(link => {
-    link.addEventListener("click", () => {
-      backdrob.classList.remove("show");
-        btnClose.classList.remove("show");
-        btnMenu.classList.remove("disabled");
-        body.classList.remove("no-scroll");
-    });
+  link.addEventListener('click', () => {
+    backdrob.classList.remove('show');
+    btnClose.classList.remove('show');
+    btnMenu.classList.remove('disabled');
+    body.classList.remove('no-scroll');
+  });
 });
-
-
 
 // switch
 
-const checkbox = document.querySelector(".switch-checkbox");
-const checkboxMob = document.querySelector(".switch-checkbox-mob");
-const svgMenu = document.querySelector(".icon-menu");
-const svgClose = document.querySelector(".icon-menu-close");
+const checkbox = document.querySelector('.switch-checkbox');
+const checkboxMob = document.querySelector('.switch-checkbox-mob');
+const svgMenu = document.querySelector('.icon-menu');
+const svgClose = document.querySelector('.icon-menu-close');
 
+checkboxMob.addEventListener('change', function () {
+  changeBackGround(checkboxMob.checked);
+  checkbox.checked = checkboxMob.checked;
+});
 
-checkboxMob.addEventListener("change", function() {
-    changeBackGround(checkboxMob.checked)
-    checkbox.checked = checkboxMob.checked;
-  });
+checkbox.addEventListener('change', function () {
+  changeBackGround(checkbox.checked);
+  checkboxMob.checked = checkbox.checked;
+});
 
-
-
-checkbox.addEventListener("change", function() {
-    changeBackGround(checkbox.checked )
-    checkboxMob.checked = checkbox.checked;
-  });
-
- export function changeBackGround (isChecked) {
-    document.body.style.backgroundColor = isChecked ? "#292929": "#f0f0f0";
-    document.body.style.color = isChecked ? "#f0f0f0": "#292929";
-    svgMenu.style.fill = isChecked ? "#f0f0f0": "#292929";
-    svgClose.style.stroke = isChecked ?" #f0f0f0": "#292929";
-    backdrob.style.backgroundColor = isChecked ? "#292929": "#f0f0f0";
-  }
-  changeBackGround(checkbox.checked || checkboxMob.checked);
+export function changeBackGround(isChecked) {
+  document.body.style.backgroundColor = isChecked ? '#292929' : '#f0f0f0';
+  document.body.style.color = isChecked ? '#f0f0f0' : '#292929';
+  svgMenu.style.fill = isChecked ? '#f0f0f0' : '#292929';
+  svgClose.style.stroke = isChecked ? ' #f0f0f0' : '#292929';
+  backdrob.style.backgroundColor = isChecked ? '#292929' : '#f0f0f0';
+  socialBoxHero.style.backgroundColor = isChecked ? '#2a2d32' : '#E4E5E6';
+  greenBoxHero.style.backgroundColor = isChecked ? '#204136' : '#BCDFD1';
+}
+changeBackGround(checkbox.checked || checkboxMob.checked);
