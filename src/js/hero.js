4b483b2f26a626332d3b7c315b43458a65e7cea1
RedisculuS <Altunina311@gmail.com>
2024-06-15 21:48:12 +0300

    1st commit new branch
    ./ changed

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,5 +1,5 @@
-import accountImgFirst from '../img/hero/account-img1x.png';
-import accountImgSecond from '../img/hero/account-img2x.png';
+import accountImgFirst from './img/hero/account-img1x.png';
+import accountImgSecond from './img/hero/account-img2x.png';
 
 document.querySelector('.accountImg').src = accountImgFirst;
 document.querySelector('.accountImg').srcset = accountImgSecond;
