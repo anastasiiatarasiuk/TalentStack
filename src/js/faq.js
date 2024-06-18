453112f3cd696c714429c50421840425cc5d9f26
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-06-18 15:33:46 +0300

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,17 +1,18 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import icon from '../img/icons.svg';
 
 // Fetch SVG sprite and inject into the DOM
-async function loadSvgSprite(url) {
-  const response = await fetch(url);
-  const sprite = await response.text();
-  const div = document.createElement('div');
-  div.style.display = 'none';
-  div.innerHTML = sprite;
-  document.body.insertBefore(div, document.body.firstChild);
-}
-
-loadSvgSprite('../img/icons.svg'); // Замініть на правильний шлях до вашого спрайту
+// async function loadSvgSprite(url) {
+//   const response = await fetch(url);
+//   const sprite = await response.text();
+//   const div = document.createElement('div');
+//   div.style.display = 'none';
+//   div.innerHTML = sprite;
+//   document.body.insertBefore(div, document.body.firstChild);
+// }
+// console.log(icon);
+// loadSvgSprite('../img/icons.svg'); // Замініть на правильний шлях до вашого спрайту
 
 // Ініціалізуємо акордеон після завантаження спрайту
 new Accordion('.accordion-container', {
@@ -25,11 +26,11 @@ new Accordion('.accordion-container', {
   onOpen: function (currentElement) {
     currentElement
       .querySelector('.faq-icon use')
-      .setAttribute('href', '#icon-Vector-up');
+      .setAttribute('href', `${icon}#icon-Vector-up`);
   },
   onClose: function (currentElement) {
     currentElement
       .querySelector('.faq-icon use')
-      .setAttribute('href', '#icon-Vector-down');
+      .setAttribute('href', `${icon}#icon-Vector-down`);
   },
 });
