a80ac1dd1f71093598b312c91eeaa21f49dc2848
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-06-18 10:46:48 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import './js/faq.js';
-import './js/form.js';
 import './js/mobile-menu.js';
 import './js/projects.js';
 import './js/hero.js';
 import './js/tech-skills';
-import './js/projects.js';
 import './js/benefits.js';
 import './js/about-me.js';
 import './js/reviews.js';
 import './js/render-projects.js';
 import './js/about-me.js';
 import './js/work-together.js';
-
+import './js/svg-utils.js';
